ebe8aef248f2e8670b4540f48da34c01ffd6cb57
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-12-02 15:49:26 -0500

    Page changes

---
+++ MobileFoodSchedules/MobileFoodSchedules/Pages/CincinnatiAccidents.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Accidents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileFoodSchedules.Pages
+{
+    public class CincinnatiAccidentsModel : PageModel
+    {
+        public CincinnatiAccident[] cincinnatiAccidents;
+        public void OnGet()
+        {
+            using (var webClient = new WebClient())
+            {
+
+
+                string cincinnatiAccidentsString = webClient.DownloadString("https://cincinnatiaccidents.azurewebsites.net/api/neighbourhoods/downtown");
+                cincinnatiAccidents = CincinnatiAccident.FromJson(cincinnatiAccidentsString);
+                ViewData["cincinnatiAccidents"] = cincinnatiAccidents;
+
+            }
+
+        }
+    }
+}
